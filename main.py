64a4f2c51aa6145c481bfab7937cf30ccf17790a
TollanBerhanu <tollanberhanu@gmail.com>
2024-03-05 15:59:39 +0300

    feat: added sample user questions

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from hyperon import MeTTa
 from biochatter.prompts import BioCypherPromptEngine
 
-# user_question="List the components of the compound 'CID432412'?"
-# user_question="What is the name of the compound with id 'CID32145'?"
-# user_question="List the descriptor attributes of the compound with id 'CID32145'?"
-# user_question="What are the descriptors of compounds?"
-# user_question="What are the sources for the parent relationship between compounds?"
-# user_question="What is the source for descriptors of compounds?"
-# user_question="What is the name of the compound with id 'CID32145'?"
-
-# user_question = "Give me the query to get the name for the compund with CID CID2499366 VALUE "
 
 prompt_engine = BioCypherPromptEngine(
                     model_name="gpt-3.5-turbo",
                     schema_config_or_info_path="./biocypher_config/schema_config.yaml",
                 )
+# user_question="What is the name of the compound with id 'CID499989'?"
+# user_question="What is the unit value for the descriptor with id 'Molecular_Weight'?"
+# user_question = "What are the descriptor ids for the compound with id 'CID2499326'?"
 user_question="What is the Exact_Mass descriptor of the compound with id 'CID2499326'? In this case you can take 'Exact_Mass' as the descriptor id"
+
 metta_query = prompt_engine.generate_query(user_question)
 print("\nLLM query:\n", metta_query)
 
