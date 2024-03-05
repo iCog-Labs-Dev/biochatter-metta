da15a881ff7d972cabb44786df64ef701ac25d1d
TollanBerhanu <tollanberhanu@gmail.com>
2024-03-05 15:12:48 +0300

    feat: run metta queries from LLM - import all metta files - append LLM metta query

--- main.py
+++ main.py
@@ -7,29 +7,31 @@ from biochatter.prompts import BioCypherPromptEngine
 # user_question="What are the descriptors of compounds?"
 # user_question="What are the sources for the parent relationship between compounds?"
 # user_question="What is the source for descriptors of compounds?"
-#user_question="What is the exact mass descriptor of the compound with id 'CID2498821'"
+# user_question="What is the name of the compound with id 'CID32145'?"
 
-
-model_name = "gpt-3.5-turbo"
-schema_config_or_info_dict = "./biocypher_config/schema_config.yaml"
-metta_file = "./metta_out/nodes.metta"
-user_question = "Give me the query to get the name for the compund with CID CID2499366 VALUE "
-
+# user_question = "Give me the query to get the name for the compund with CID CID2499366 VALUE "
 
 prompt_engine = BioCypherPromptEngine(
-            model_name=model_name,
-            schema_config_or_info_path=schema_config_or_info_dict,
-        )
-
-
+                    model_name="gpt-3.5-turbo",
+                    schema_config_or_info_path="./biocypher_config/schema_config.yaml",
+                )
+user_question="What is the Exact_Mass descriptor of the compound with id 'CID2499326'? In this case you can take 'Exact_Mass' as the descriptor id"
 metta_query = prompt_engine.generate_query(user_question)
-
-# query_func = agent.get_query_results
-print("\n\nLLM query:\n")
-print(metta_query)
-
+print("\nLLM query:\n", metta_query)
 
 metta = MeTTa()
-print("\n\nMeTTa runtime:\n")
-print(metta.import_file(metta_file))
 
+def read_metta_file(filename):
+    with open(filename, 'r') as file:
+        file_content = file.read()
+    return str(file_content)
+
+metta_sample = f"""\
+{read_metta_file('./metta_out/compound_nodes.metta')}\
+{read_metta_file('./metta_out/compound_edges.metta')}\
+{read_metta_file('./metta_out/descriptor_nodes.metta')}\
+
+{str(metta_query).strip()}
+"""
+
+print("\nMeTTa output:\n", metta.run(metta_sample))
